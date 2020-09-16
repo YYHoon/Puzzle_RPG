0af198a893249502b06ecfe76f473eb9c566b4a6
Cathoon <cathoon94@naver.com>
2020-09-17 03:01:00 +0900

    Procedural Cave Generation

---
+++ Puzzle_RPG/Puzzle_RPG/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net.Security;
+using UnityEngine.UIElements;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public string seed;
+    public bool useRnadomSeed;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+    int[,] map;
+    private void Start()
+    {
+        Generator();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Generator();
+        }
+    }
+
+    void Generator()
+    {
+        map = new int[width, height];
+        RandomFillMpa();
+        for(int i=0;i<5;++i)
+        {
+            SmoothMap();
+        }
+
+        ProcesMap();
+        int borderSize = 1;
+        int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
+        for (int x = 0; x < borderedMap.GetLength(0); ++x)
+        {
+            for (int y = 0; y < borderedMap.GetLength(1); ++y)
+            {
+                if(x>=borderSize && x<width+borderSize&&y>=borderSize && y <height +borderSize)
+                {
+                    borderedMap[x, y] = map[x - borderSize, y - borderSize];
+                }
+                else
+                {
+                    borderedMap[x, y] = 1;
+                }
+            }
+        }
+                MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(borderedMap, 1);
+    }
+
+    void ProcesMap()
+    {
+        List<List<Coord>> wallRegions = GetRegions(1);
+        int wallThresholdSize = 50;
+        foreach (List<Coord> wallRegion in wallRegions)
+        {
+            if (wallRegion.Count < wallThresholdSize)
+            {
+                foreach(Coord tile in wallRegion)
+                {
+                    map[tile.tileX, tile.tileY] = 0;
+                }
+            }
+        }
+
+        List<List<Coord>> RoomRegions = GetRegions(0);
+        int roomThresholdSize = 50;
+        foreach (List<Coord> RoomRegion in RoomRegions)
+        {
+            if (RoomRegion.Count < roomThresholdSize)
+            {
+                foreach (Coord tile in RoomRegion)
+                {
+                    map[tile.tileX, tile.tileY] = 1;
+                }
+            }
+        }
+    }
+
+    List<List<Coord>> GetRegions(int tileType)
+    {
+        List<List<Coord>> regions = new List<List<Coord>>();
+        int[,] mapFlags = new int[width, height];
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                if(mapFlags[x,y]==0&& map[x,y]==tileType)
+                {
+                    List<Coord> newRegion = GetRegionTiles(x, y);
+                    regions.Add(newRegion);
+                    foreach(Coord tile in newRegion)
+                    {
+                        mapFlags[tile.tileX, tile.tileY] = 1;
+                    }
+                }
+            }
+        }
+        return regions;
+    }
+
+    List<Coord> GetRegionTiles(int startX, int startY)
+    {
+        List<Coord> tiles = new List<Coord>();
+        int [,] mapFlags = new int[width, height];
+        int tileType = map[startX, startY];
+
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(new Coord(startX, startY));
+        mapFlags[startX, startY] = 1;
+
+        while(queue.Count>0)
+        {
+            Coord tile = queue.Dequeue();
+            tiles.Add(tile);
+
+            for(int x = tile.tileX-1;x<=tile.tileX +1;++x)
+            {
+                for(int y = tile.tileY-1;y<=tile.tileY +1;++y)
+                {
+                    if(IsInMapRange(x,y)&& (y==tile.tileY || x == tile.tileX))
+                    {
+                        if (mapFlags[x,y] == 0 && map[x,y]==tileType)
+                        {
+                            mapFlags[x, y] = 1;
+                            queue.Enqueue(new Coord(x, y));
+                        }
+                    }
+                }
+            }
+        }
+        return tiles;
+    }
+
+    bool IsInMapRange(int x,int y)
+    {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+    void RandomFillMpa()
+    {
+        if(useRnadomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+        
+        for(int x = 0;x<width;++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
+                if (neighbourWallTiles > 5)
+                {
+                    map[x, y] = 1;
+                }
+                else if (neighbourWallTiles < 4)
+                {
+                    map[x, y] = 0;
+                }
+                
+            }
+        }
+    }
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for(int neighbourX = gridX-1;neighbourX<=gridX +1; ++neighbourX)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; ++neighbourY)
+            {
+                if (IsInMapRange(neighbourX,neighbourY))
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                else
+                {
+                    ++wallCount;
+                }
+            }
+        }
+        return wallCount;
+    }
+    struct Coord
+    {
+        public int tileX;
+        public int tileY;
+
+        public Coord(int x,int y)
+        {
+            tileX = x;
+            tileY = y;
+        }
+    }
+}
