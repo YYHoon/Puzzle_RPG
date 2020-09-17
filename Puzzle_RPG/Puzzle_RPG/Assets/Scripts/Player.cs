ec1a6af14137c6be8222b1b9f31b6e2990d57f25
Cathoon <cathoon94@naver.com>
2020-09-18 01:45:32 +0900

    Procedural Cave Generation, 카메라 플레이어 간단하게 넣어봤습니다

---
+++ Puzzle_RPG/Puzzle_RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * 10;
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+    }
+    
+}
